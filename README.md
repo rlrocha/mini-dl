392cb9dca88d88386cce893f2c4b8b5dc0697f8a
Rafael de Lima Rocha <51142422+rlrocha@users.noreply.github.com>
2021-07-05 21:46:23 -0300

    Update

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Minicurso Aprendizado Profundo e Redes Neurais Convolucionais
 
-Repositório com os exemplos utilizados no mini curso de aprendizado profundo e redes neurais convolucionais realizados na Universidade Federal do Pará (UFPA) em 2019.
+Repositório com os exemplos utilizados no minicurso de aprendizado profundo e redes neurais convolucionais realizados na Universidade Federal do Pará (UFPA), campos Tucuruí (CAMTUC) em 2019.
+
+## Exemplos
+
+- [Exemplo 1](exemplo1_activations.py): Funções de ativação.
+- [Exemplo 2](exemplo2_xorIO.py): Entradas e saídas porta lógica XOR.
+- [Exemplo 3](exemplo3_xor.py): MLP para porta lógica XOR.
+- [Exemplo 4](exemplo4_xorSGD.py): MLP Tensorflow para porta lógica XOR.
+- [Exemplo 5](exemplo5_logic.py): MLP Tensorflow para portas lógicas.
+- [Exemplo 6](exemplo6_softmax.py): Função de ativação softmax.
+- [Exemplo 7](exemplo7_banknotes.py): MLP Tensorflow para a base banknotes.
+- [Exemplo 8](exemplo8_iris.py): MLP Tensorflow para a base iris.
+- [Exemplo 9](exemplo9_digitis.py): MLP Tensorflow para a base digits.
+- [Exemplo 10](exemplo10_convparams.py): Parâmetros CNN.
+- [Exemplo 11](exemplo11_digitis.py): CNN Tensorflow para a base digits.
+- [Exemplo 12](exemplo12_lenet5.py): Arquitetura Lenet5.
+- [Exemplo 13](exemplo13_layers.py): Saídas das camadas da CNN com a base mnist.
+
+Os slides do minicurso estão disponíveis no [link](https://mega.nz/file/WB8HwQrb#AQD8JsvRrER4QlAip-hu5e06v2FrlGHoVmkYrLpAnBU).
---
+++ exemplo4_xorSGD.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 18:36:34 2019
+
+@author: Rafael Rocha
+"""
+
+#%% Pacotes utilizados
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+import keras.backend as kb
+
+#%% Funções
+def sse (t, y):
+    
+    """
+    Sum-of-squares error
+    """
+    
+    error = kb.square(y - t)
+    
+    return error
+
+def mse (t, y):
+    
+    """
+    Mean squared error
+    """
+    
+    error = kb.mean(kb.square(y - t), axis=-1)
+    
+    return error
+
+#%% Parâmetros iniciais
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+t = np.array([[0],[1],[1],[0]])
+
+D = x.shape[1]
+M = 5
+K = 1
+
+print(np.concatenate([x,t], axis=1))
+
+#%% Criação da rede
+model = Sequential()
+
+model.add(Dense(M, input_shape=(D,), activation='tanh'))
+model.add(Dense(K, activation='sigmoid'))
+
+initial_weights = model.get_weights()
+
+model.compile(SGD(lr=0.5),
+              loss=[mse]
+              )
+h = model.fit(x.astype(np.float32), t.astype(np.float32), epochs=500, verbose=0)
+
+model.summary()
+
+#%% Plots
+y = model.predict(x)
+
+x_temp = np.arange(1, np.size(h.history['loss'])+1)
+
+plt.figure()
+plt.plot(x_temp, h.history['loss'])
+plt.ylabel('Erro')
+plt.xlabel('Época')
+
+plt.figure()
+plt.plot(t,'o', y, 'o')
+plt.xlabel('Exemplos')
+plt.ylabel('Saídas')
+plt.legend(['Saída esperada', 'Saída da rede'])
+plt.axhline(0.5, color='black')
